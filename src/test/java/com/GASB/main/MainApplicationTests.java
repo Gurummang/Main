650055eca94f8856f9dea1e24b24853cfb4763c6
Yubin <qls773322@naver.com>
2024-09-27 15:49:53 +0900

    Springboot initialize setting

---
+++ src/test/java/com/GASB/main/MainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GASB.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
